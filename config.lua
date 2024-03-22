4b14cb350e73ae273fbbae23b47406172a0e36f8
Fire Scripts <163156589+FireScripts2@users.noreply.github.com>
2024-03-22 14:00:53 +0100

    Delete config.lua

--- config.lua
+++
@@ -1,9 +0,0 @@
-Config = {}
-
-Config.zones = {
-    {
-        coords = vec3(1847.2003, 3659.1311, 34.2237), -- itt állíthatod a safezone kordinátáit (vec3)!
-        radius = 30.0,                                -- itt állíthatod a safezone körsugarának méretét!
-        debug = true,                                -- debug = false vagy true!
-    },
-}
