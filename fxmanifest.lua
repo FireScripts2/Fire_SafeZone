f83cce3a256f3a4667b539283eb98caa0e82b896
Fire Scripts <163156589+FireScripts2@users.noreply.github.com>
2024-03-22 14:02:22 +0100

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,20 @@
+fx_version 'adamant'
+
+game 'gta5'
+
+lua54 'yes'
+
+description 'safezone'
+
+author 'loading'
+
+version '1.0'
+
+client_script {
+    'client/*.lua'
+}
+
+shared_script {
+    'config/config.lua',
+    '@ox_lib/init.lua'
+}
